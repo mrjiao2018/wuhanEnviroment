0f93969d65ea4d7eccdd4108b8cff69f0a319b19
mrjiao <mrjiao2018@163.com>
2018-07-03 17:14:42 +0800

    前端静态资源文件整合重构

---
+++ src/main/java/servlet/NewServletTest.java
@@ -0,0 +1,30 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class NewServletTest extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+        resp.setContentType("text/html; charset=utf-8");
+//        ServletInputStream inputStream = req.getInputStream();
+//        int len = -1;
+//        byte[] buffer = new byte[1024];
+//        while((len = inputStream.read(buffer)) != -1) {
+//            System.out.println(buffer);
+//        }
+        System.out.println(req.getParameter("username"));
+        System.out.println(req.getParameter("password"));
+        resp.getWriter().write("{\"status\":600}");
+    }
+}
