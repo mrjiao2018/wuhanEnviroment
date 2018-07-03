04440e02862b4db3a193d06695d21ea5967a1f7b
mrjiao <mrjiao2018@163.com>
2018-07-03 11:57:15 +0800

    index界面函数完善

--- src/main/webapp/statics/index/js/index.js
+++ src/main/webapp/statics/index/js/index.js
@@ -1,3 +1,24 @@
+var form_data;
+//加载网页前向服务器申请数据
+$(window).load(function () {
+    var request_data = {
+        year:2011,
+        position:"liangzilake",
+        type:"forest"
+    };
+    //todo finish the url
+    // function test
+    // var options = {
+    //     url:"",
+    //     data:JSON.stringify(request_data),
+    //     dataType:"json",
+    //     success:function (data) {
+    //         form_data = data;
+    //     }
+    // };
+    // $.ajax(options);
+});
+
 $(function () {
     //获取所有的form对象
     var calculatorArr = [];
@@ -11,20 +32,28 @@ $(function () {
         var $specificCalcForm = $specificCalcDiv.find("form").eq(0);
         calculatorArr.push($specificCalcForm);
     }
-
     //获取总表
     var $summarySheet = $(".summary-sheet").eq(0);
 
+    //获取日期
+    var year = $("#year").text();
+
+    //todo 加载界面时，将所有数据加载到表单中
+    dataLoad(year, calculatorArr, $summarySheet);
+
     //在加载界面时，只展示第一个calcDiv，隐藏其余div及总表
-    //showSingleDiv(calcDivArr, $summarySheet);
+    showSingleDiv(calcDivArr, $summarySheet, 0);
 
     //左侧导航栏切换表单函数
-    switchForm();
+    switchForm(calcDivArr, $summarySheet);
 
     //提交表单
     for(var k = 0; k < calculatorArr.length; ++k) {
         submitForm(calculatorArr[k]);
     }
+
+    //更改日期时，重新加载data并刷新界面
+    dataReload(calculatorArr, $summarySheet);
 });
 
 //表单提交函数
@@ -46,17 +75,75 @@ function submitForm($form) {
     $form.ajaxForm(options)
 }
 
-//在加载界面时，只展示第一个calcDiv，隐藏其余div及总表
-function showSingleDiv(calcDivArr, $summarySheet) {
-    for(var j = 1; j < calcDivArr.length; ++j) {
-        calcDivArr[j].addClass("hidden");
+//只展示一个calcDiv，隐藏其余div及总表
+function showSingleDiv(calcDivArr, $summarySheet, index) {
+    for(var j = 0; j < calcDivArr.length; ++j) {
+        if(j == index){
+            calcDivArr[j].removeClass("hidden");
+        }
+        else{
+            calcDivArr[j].addClass("hidden");
+        }
     }
-    $summarySheet.addClass("hidden");
+    if(index == calcDivArr.length) {
+        $summarySheet.removeClass("hidden");
+    }
+    else{
+        $summarySheet.addClass("hidden");
+    }
 }
 
 //左侧导航栏切换表单函数
-function switchForm() {
+function switchForm(calcDivArr, $summarySheet) {
+    var $side_navs = $(".side-nav");
+    for( var i = 0; i < $side_navs.length; ++i){
+        $side_navs[i].index = i;
+        $side_navs.eq(i).click(function () {
+            showSingleDiv(calcDivArr, $summarySheet, this.index);
+        });
+    }
+}
 
+//todo
+function showData(data, calculatorArr, $summarySheet) {
+
+}
+
+//加载data数据并显示在form中
+function dataLoad(year, calculatorArr, $summarySheet) {
+    var request_data = {
+        year:year,
+        position:"liangzilake",
+        type:"forest"
+    };
+    var options = {
+        url:"",
+        data:JSON.stringify(request_data),
+        dataType:"json",
+        success:function(data){
+            showData(data, calculatorArr, $summarySheet);
+        }
+    };
+    $.ajax(options);
+}
+
+//更改日期表单重新加载函数
+function dataReload(calculatorArr, $summarySheet) {
+    //更改日期的处理
+    var $year_select = $("#year-select");
+    var $year_options = $year_select.find("dd>a");
+    var $year = $("#year");
+    var year;
+    //改变主界面中的日期显示
+    for(var i = 0; i < $year_options.length; ++i) {
+        $year_options.eq(i).click(function () {
+            year = $(this).text();
+            $year.text(year);
+            $year_select.find("#year-selected").text(year);
+            //加载data数据并显示在form中
+            dataLoad(year, calculatorArr, $summarySheet);
+        });
+    }
 }
 
 
